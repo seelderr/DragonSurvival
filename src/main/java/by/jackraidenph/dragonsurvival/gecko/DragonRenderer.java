9b843f22f8650a97bd58538014b23c56731c52fa
HH <haavard@hoijord.net>
2021-11-25 23:29:51 +0100

    Update DragonRenderer.java

--- src/main/java/by/jackraidenph/dragonsurvival/gecko/DragonRenderer.java
+++ src/main/java/by/jackraidenph/dragonsurvival/gecko/DragonRenderer.java
@@ -6,9 +6,12 @@ import com.mojang.blaze3d.vertex.IVertexBuilder;
 import net.minecraft.client.renderer.IRenderTypeBuffer;
 import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.entity.EntityRendererManager;
+import net.minecraft.client.renderer.texture.OverlayTexture;
 import net.minecraft.util.ResourceLocation;
 import software.bernie.geckolib3.model.AnimatedGeoModel;
 import software.bernie.geckolib3.renderers.geo.GeoEntityRenderer;
+import software.bernie.geckolib3.renderers.geo.GeoLayerRenderer;
+import software.bernie.geckolib3.renderers.geo.IGeoRenderer;
 
 import javax.annotation.Nullable;
 import java.awt.*;
@@ -18,8 +21,10 @@ public class DragonRenderer extends GeoEntityRenderer<DragonEntity> {
 	
 	public DragonRenderer(EntityRendererManager renderManager, AnimatedGeoModel<DragonEntity> modelProvider) {
         super(renderManager, modelProvider);
+		this.addLayer(new glowLayerRender(this));
     }
 	
+<<<<<<< Updated upstream
 	@Override
 	public void render(DragonEntity entity, float entityYaw, float partialTicks, MatrixStack stack, IRenderTypeBuffer bufferIn, int packedLightIn)
 	{
@@ -33,21 +38,47 @@ public class DragonRenderer extends GeoEntityRenderer<DragonEntity> {
 			renderType = null;
 		}
 	}
+=======
+>>>>>>> Stashed changes
 	
 	public Color renderColor = new Color(255, 255, 255);
-	public RenderType renderType;
-    @Override
-    public RenderType getRenderType(DragonEntity animatable, float partialTicks, MatrixStack stack,
-			@Nullable IRenderTypeBuffer renderTypeBuffer, @Nullable IVertexBuilder vertexBuilder, int packedLightIn,
-			ResourceLocation textureLocation) {
-		return renderType == null ? RenderType.entityCutout(textureLocation) : renderType;
-	}
-
+	
 	@Override
 	public Color getRenderColor(DragonEntity animatable, float partialTicks, MatrixStack stack,
 			@Nullable IRenderTypeBuffer renderTypeBuffer,
 			@Nullable IVertexBuilder vertexBuilder, int packedLightIn)
 	{
 		return renderColor;
+	}
+	public class glowLayerRender extends GeoLayerRenderer<DragonEntity>
+	{
+		private final IGeoRenderer<DragonEntity> renderer;
+		
+		public glowLayerRender(IGeoRenderer<DragonEntity> entityRendererIn)
+		{
+			super(entityRendererIn);
+			this.renderer = entityRendererIn;
+		}
+		
+		@Override
+		public void render(MatrixStack matrixStackIn, IRenderTypeBuffer bufferIn, int packedLightIn, DragonEntity entitylivingbaseIn, float limbSwing, float limbSwingAmount, float partialTicks, float ageInTicks, float netHeadYaw, float headPitch)
+		{
+			if (glowTexture != null) {
+				RenderType type = RenderType.eyes(glowTexture);
+				IVertexBuilder vertexConsumer = bufferIn.getBuffer(type);
+				
+				renderer.render(getEntityModel().getModel(getEntityModel().getModelLocation(entitylivingbaseIn)),
+				                entitylivingbaseIn,
+				                partialTicks,
+				                type,
+				                matrixStackIn,
+				                bufferIn,
+				                vertexConsumer,
+				                0,
+				                OverlayTexture.NO_OVERLAY,
+				                1.0F, 1.0F, 1.0F, 1.0F
+				);
+			}
+		}
 	}
 }
