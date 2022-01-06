cd6431b64c48df55c9e1033dc773339a35093a5b
Black Aures <angelochek517@mail.ru>
2022-01-06 13:27:32 +0300

    Aures is sorted

--- src/main/java/by/jackraidenph/dragonsurvival/common/items/DragonSurvivalCreativeTab.java
+++ src/main/java/by/jackraidenph/dragonsurvival/common/items/DragonSurvivalCreativeTab.java
@@ -28,7 +28,18 @@ public class DragonSurvivalCreativeTab extends ItemGroup
 	public void fillItemList(NonNullList<ItemStack> items)
 	{
 		super.fillItemList(items);
-		List<IItemProvider> list = Arrays.asList(DSItems.starBone, DSBlocks.PREDATOR_STAR_BLOCK);
+		List<IItemProvider> list = Arrays.asList(
+				DSBlocks.dragon_altar_stone, DSBlocks.dragon_altar_sandstone, DSBlocks.dragon_altar_red_sandstone, DSBlocks.dragon_altar_purpur_block,
+				DSBlocks.dragon_altar_oak_log, DSBlocks.dragon_altar_nether_bricks, DSBlocks.dragon_altar_mossy_cobblestone, DSBlocks.dragon_altar_blackstone,
+				DSBlocks.dragon_altar_birch_log, DSItems.starBone, DSItems.starHeart, DSItems.elderDragonDust, DSItems.elderDragonBone, DSItems.dragonHeartShard,
+				DSItems.weakDragonHeart, DSItems.elderDragonHeart, DSBlocks.PREDATOR_STAR_BLOCK, DSBlocks.dragonMemoryBlock, DSBlocks.dragonBeacon, DSBlocks.peaceDragonBeacon,
+				DSBlocks.magicDragonBeacon, DSBlocks.fireDragonBeacon, DSBlocks.forestSourceOfMagic, DSBlocks.caveSourceOfMagic, DSBlocks.seaSourceOfMagic,
+				DSItems.wingGrantItem, DSItems.spinGrantItem, DSItems.seaDragonTreat, DSItems.forestDragonTreat, DSItems.caveDragonTreat,
+				DSItems.charredMeat, DSItems.charredVegetable, DSItems.charredMushroom, DSItems.charredSeafood, DSItems.chargedCoal, DSItems.chargedSoup,
+				DSBlocks.treasureDebris, DSBlocks.treasureDiamond, DSBlocks.treasureEmerald, DSBlocks.treasureCopper, DSBlocks.treasureGold, DSBlocks.treasureIron,
+				DSBlocks.helmet2, DSBlocks.helmet1, DSBlocks.helmet3, DSBlocks.caveDoor, DSBlocks.forestDoor, DSBlocks.seaDoor, DSBlocks.spruceDoor,
+				DSBlocks.acaciaDoor, DSBlocks.birchDoor, DSBlocks.jungleDoor, DSBlocks.oakDoor, DSBlocks.darkOakDoor, DSBlocks.crimsonDoor,
+				DSBlocks.warpedDoor, DSBlocks.legacyDoor, DSBlocks.ironDoor, DSBlocks.murdererDoor, DSBlocks.sleeperDoor, DSBlocks.stoneDoor);
 		Comparator<ItemStack> c = Ordering.explicit(list).onResultOf(ItemStack::getItem);
 		items.sort((c1, c2) -> list.contains(c1.getItem()) && !list.contains(c2.getItem()) ? -1 :
 				!list.contains(c1.getItem()) && list.contains(c2.getItem()) ? 1 :
