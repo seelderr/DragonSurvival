68760fee7cf56f2c4a66df6393cdb30327b9c6c7
HH <haavard@hoijord.net>
2022-03-27 00:01:22 +0100

    fix: Fixes #126

--- src/main/java/by/dragonsurvivalteam/dragonsurvival/common/entity/creatures/hitbox/DragonHitBox.java
+++ src/main/java/by/dragonsurvivalteam/dragonsurvival/common/entity/creatures/hitbox/DragonHitBox.java
@@ -226,7 +226,7 @@ public class DragonHitBox extends MobEntity{
 
 	@Override
 	public boolean isInvulnerableTo(DamageSource pSource){
-		return super.isInvulnerableTo(pSource) || pSource == DamageSource.IN_WALL || pSource == DamageSource.LIGHTNING_BOLT || player.isInvulnerableTo(pSource);
+		return super.isInvulnerableTo(pSource) || pSource == DamageSource.IN_WALL || pSource == DamageSource.LIGHTNING_BOLT || player != null && player.isInvulnerableTo(pSource);
 	}
 
 	@Override
