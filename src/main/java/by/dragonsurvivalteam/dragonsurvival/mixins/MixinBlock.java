64ac0e6c0962bde8b49176380caba164f556b523
Horeak <haavard@hoijord.net>
2022-07-07 18:21:11 +0200

    fix: Aures, stop giving me more work. I have enough!

--- src/main/java/by/dragonsurvivalteam/dragonsurvival/mixins/MixinBlock.java
+++ src/main/java/by/dragonsurvivalteam/dragonsurvival/mixins/MixinBlock.java
@@ -14,30 +14,28 @@ import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import javax.annotation.Nullable;
-import java.util.List;
 
 @Mixin( Block.class )
 public class MixinBlock{
 	@Inject( at = @At( "HEAD" ), method = "dropResources(Lnet/minecraft/world/level/block/state/BlockState;Lnet/minecraft/world/level/Level;Lnet/minecraft/core/BlockPos;Lnet/minecraft/world/level/block/entity/BlockEntity;Lnet/minecraft/world/entity/Entity;Lnet/minecraft/world/item/ItemStack;)V", cancellable = true )
 	private static void dropResources(BlockState p_220054_0_, Level p_220054_1_, BlockPos p_220054_2_,
 		@Nullable
-			BlockEntity p_220054_3_, Entity entity, ItemStack p_220054_5_, CallbackInfo ci){
+		BlockEntity p_220054_3_, Entity entity, ItemStack p_220054_5_, CallbackInfo ci){
 		if(!DragonUtils.isDragon(entity)){
 			return;
 		}
 		DragonStateHandler handler = DragonUtils.getHandler(entity);
-		if(handler == null || handler.getType() != DragonType.CAVE){
+		if(handler.getType() != DragonType.CAVE){
 			return;
 		}
 
 		if(p_220054_1_ instanceof ServerLevel){
-			getDrops(p_220054_0_, (ServerLevel)p_220054_1_, p_220054_2_, p_220054_3_, entity, p_220054_5_).forEach((p_220057_2_) -> {
+			Block.getDrops(p_220054_0_, (ServerLevel)p_220054_1_, p_220054_2_, p_220054_3_, entity, p_220054_5_).forEach((p_220057_2_) -> {
 				if(!p_220054_1_.isClientSide && !p_220057_2_.isEmpty() && p_220054_1_.getGameRules().getBoolean(GameRules.RULE_DOBLOCKDROPS) && !p_220054_1_.restoringBlockSnapshots){
 					float f = 0.5F;
 					double d0 = (double)(p_220054_1_.random.nextFloat() * 0.5F) + 0.25D;
@@ -57,14 +55,5 @@ public class MixinBlock{
 		}
 
 		ci.cancel();
-	}
-
-	@Shadow
-	public static List<ItemStack> getDrops(BlockState p_220077_0_, ServerLevel p_220077_1_, BlockPos p_220077_2_,
-		@Nullable
-			BlockEntity p_220077_3_,
-		@Nullable
-			Entity p_220077_4_, ItemStack p_220077_5_){
-		throw new IllegalStateException("Mixin failed to shadow getDrops()");
 	}
 }
