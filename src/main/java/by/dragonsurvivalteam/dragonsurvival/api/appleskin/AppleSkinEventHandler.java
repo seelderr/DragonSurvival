b8293b88829c251e4a6a3fbc1c0d1fc71c449106
Kai <kai.zahn@yahoo.de>
2023-06-09 00:21:16 +0200

    Fixed rotten tooltip

--- src/main/java/by/dragonsurvivalteam/dragonsurvival/api/appleskin/AppleSkinEventHandler.java
+++ src/main/java/by/dragonsurvivalteam/dragonsurvival/api/appleskin/AppleSkinEventHandler.java
@@ -1,7 +1,11 @@
 package by.dragonsurvivalteam.dragonsurvival.api.appleskin;
 
 import by.dragonsurvivalteam.dragonsurvival.api.DragonFood;
+import by.dragonsurvivalteam.dragonsurvival.util.DragonUtils;
 import net.minecraft.client.Minecraft;
+import net.minecraft.world.effect.MobEffect;
+import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.food.FoodProperties;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
@@ -9,16 +13,41 @@ import net.minecraftforge.eventbus.api.SubscribeEvent;
 import squeek.appleskin.api.event.FoodValuesEvent;
 import squeek.appleskin.api.food.FoodValues;
 
+import java.util.function.Supplier;
+
 @OnlyIn(Dist.CLIENT)
 public class AppleSkinEventHandler {
     @SubscribeEvent
     public void onToolTipEvent(final FoodValuesEvent event) {
-        FoodProperties properties = DragonFood.getEffectiveFoodProperties(event.itemStack.getItem(), Minecraft.getInstance().player);
-
-        if (properties == null) {
+        if (!DragonUtils.isDragon(event.player)) {
             return;
         }
 
-        event.modifiedFoodValues = new FoodValues(properties.getNutrition(), properties.getSaturationModifier());
+        FoodProperties foodProperties = DragonFood.getEffectiveFoodProperties(event.itemStack.getItem(), Minecraft.getInstance().player);
+
+        if (foodProperties == null) {
+            return;
+        }
+
+        // TODO :: remove (+ accesstransformer part)
+        // This also removes those effects from the food
+//        if (foodProperties.getNutrition() > 0) {
+//            FoodProperties itemFoodProperties = event.itemStack.getItem().getFoodProperties(event.itemStack, event.player);
+//            List<Pair<Supplier<MobEffectInstance>, Float>> effects = itemFoodProperties.effects;
+//
+//            // Could also do: !(pair.getFirst().get().getEffect().getCategory() == MobEffectCategory.HARMFUL))
+//            effects = effects.stream()
+//                    .filter(pair -> isEffectRelevant(pair.getFirst()))
+//                    .toList();
+//
+//            itemFoodProperties.effects = effects;
+//        }
+
+        event.modifiedFoodValues = new FoodValues(foodProperties.getNutrition(), foodProperties.getSaturationModifier());
+    }
+
+    private boolean isEffectRelevant(final Supplier<MobEffectInstance> first) {
+        MobEffect effect = first.get().getEffect();
+        return !effect.equals(MobEffects.HUNGER) && !effect.equals(MobEffects.POISON);
     }
 }
