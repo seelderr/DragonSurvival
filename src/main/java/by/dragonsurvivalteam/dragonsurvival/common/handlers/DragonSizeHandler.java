1038b47473dc9348a8dc60ebe0e58fcc46379f8f
Kai <kai.zahn@yahoo.de>
2024-01-12 19:24:27 +0100

    don't clear forced pose every tick - fixes #412

--- src/main/java/by/dragonsurvivalteam/dragonsurvival/common/handlers/DragonSizeHandler.java
+++ src/main/java/by/dragonsurvivalteam/dragonsurvival/common/handlers/DragonSizeHandler.java
@@ -12,7 +12,6 @@ import net.minecraft.world.entity.EntityDimensions;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.Pose;
 import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.phys.AABB;
 import net.minecraftforge.event.TickEvent;
 import net.minecraftforge.event.entity.EntityEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -28,12 +27,17 @@ public class DragonSizeHandler{
 
 	@SubscribeEvent
 	public static void getDragonSize(EntityEvent.Size event){
-		if(!(event.getEntity() instanceof Player player) || !DragonUtils.isDragon(player)){
+		if (!(event.getEntity() instanceof Player player)) {
 			return;
 		}
-		DragonStateHandler dragonStateHandler = DragonUtils.getHandler(player);
 
-		double size = dragonStateHandler.getSize();
+		DragonStateHandler handler = DragonUtils.getHandler(player);
+
+		if (!handler.isDragon()) {
+			return;
+		}
+
+		double size = handler.getSize();
 		// Calculate base values
 		double height = calculateDragonHeight(size, ServerConfig.hitboxGrowsPastHuman);
 		double width = calculateDragonWidth(size, ServerConfig.hitboxGrowsPastHuman);
@@ -107,10 +111,8 @@ public class DragonSizeHandler{
 	}
 
 	public static Pose getOverridePose(LivingEntity player){
-		DragonStateHandler handler = DragonUtils.getHandler(player);
-
 		if(player != null){
-			boolean swimming = (player.isInWaterOrBubble() || player.isInLava() && ServerConfig.bonuses && ServerConfig.caveLavaSwimming && DragonUtils.isDragonType(handler, DragonTypes.CAVE)) && player.isSprinting() && !player.isPassenger();
+			boolean swimming = (player.isInWaterOrBubble() || player.isInLava() && ServerConfig.bonuses && ServerConfig.caveLavaSwimming && DragonUtils.isDragonType(player, DragonTypes.CAVE)) && player.isSprinting() && !player.isPassenger();
 			boolean flying = ServerFlightHandler.isFlying(player);
 			boolean spinning = player.isAutoSpinAttack();
 			boolean crouching = player.isShiftKeyDown();
