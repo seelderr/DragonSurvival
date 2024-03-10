2c3272ddc78c9d3431d5186c1ed3de6caa0acc89
Kai <kai.zahn@yahoo.de>
2024-03-10 22:12:07 +0100

    fix lan crash

--- src/main/java/by/dragonsurvivalteam/dragonsurvival/client/emotes/EmoteHandler.java
+++ src/main/java/by/dragonsurvivalteam/dragonsurvival/client/emotes/EmoteHandler.java
@@ -14,12 +14,12 @@ import net.minecraft.world.entity.ai.attributes.AttributeModifier;
 import net.minecraft.world.entity.ai.attributes.Attributes;
 import net.minecraft.world.entity.player.Player;
 import net.minecraftforge.api.distmarker.Dist;
-import net.minecraftforge.api.distmarker.OnlyIn;
+import net.minecraftforge.event.TickEvent;
 import net.minecraftforge.event.TickEvent.ClientTickEvent;
 import net.minecraftforge.event.TickEvent.Phase;
-import net.minecraftforge.event.TickEvent.PlayerTickEvent;
 import net.minecraftforge.event.entity.living.LivingHurtEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.LogicalSide;
 import net.minecraftforge.fml.common.Mod;
 
 import java.util.Arrays;
@@ -28,121 +28,118 @@ import java.util.UUID;
 
 import static net.minecraft.client.CameraType.THIRD_PERSON_BACK;
 
+@Mod.EventBusSubscriber(Dist.CLIENT)
+public class EmoteHandler {
+    private static final UUID EMOTE_NO_MOVE = UUID.fromString("09c2716e-0da9-430b-aaaf-8653c643dc09");
 
-@OnlyIn( Dist.CLIENT )
-@Mod.EventBusSubscriber( Dist.CLIENT )
-public class EmoteHandler{
-	private static final UUID EMOTE_NO_MOVE = UUID.fromString("09c2716e-0da9-430b-aaaf-8653c643dc09");
+    @SubscribeEvent
+    public static void playerTick(final TickEvent.PlayerTickEvent event) {
+        if (event.phase == Phase.START || /* Avoid issues with LAN */ event.side == LogicalSide.SERVER) {
+            return;
+        }
 
-	@OnlyIn( Dist.CLIENT )
-	@SubscribeEvent
-	public static void playerTick(PlayerTickEvent event){
-		if(event.phase == Phase.START){
-			return;
-		}
+        Player player = event.player;
 
-		Player player = event.player;
+        if (player != null) {
+            DragonStateProvider.getCap(player).ifPresent(cap -> {
+                for (int index = 0; index < cap.getEmoteData().currentEmotes.length; index++) {
+                    Emote emote = cap.getEmoteData().currentEmotes[index];
 
-		if(player != null){
-			DragonStateProvider.getCap(player).ifPresent(cap -> {
-				for(int index = 0; index < cap.getEmoteData().currentEmotes.length; index++){
-					Emote emote = cap.getEmoteData().currentEmotes[index];
+                    if (emote == null) {
+                        cap.getEmoteData().emoteTicks[index] = 0;
+                        continue;
+                    }
 
-					if(emote == null){
-						cap.getEmoteData().emoteTicks[index] = 0;
-						continue;
-					}
+                    if (cap.getEmoteData().emoteTicks.length < index || cap.getEmoteData().emoteTicks[index] == null) {
+                        cap.getEmoteData().emoteTicks[index] = 0;
+                    }
 
-					if(cap.getEmoteData().emoteTicks.length < index || cap.getEmoteData().emoteTicks[index] == null){
-						cap.getEmoteData().emoteTicks[index] = 0;
-					}
+                    cap.getEmoteData().emoteTicks[index] += 1;
 
-					cap.getEmoteData().emoteTicks[index] += 1;
+                    //Cancel emote if its duration is expired, this should happen even if it isnt local
+                    if (emote.duration != -1 && cap.getEmoteData().emoteTicks[index] > emote.duration) {
+                        cap.getEmoteData().currentEmotes[index] = null;
+                        cap.getEmoteData().emoteTicks[index] = 0;
+                        NetworkHandler.CHANNEL.sendToServer(new SyncEmote(player.getId(), cap.getEmoteData()));
+                        break;
+                    }
 
-					//Cancel emote if its duration is expired, this should happen even if it isnt local
-					if(emote.duration != -1 && cap.getEmoteData().emoteTicks[index] > emote.duration){
-						cap.getEmoteData().currentEmotes[index] = null;
-						cap.getEmoteData().emoteTicks[index] = 0;
-						NetworkHandler.CHANNEL.sendToServer(new SyncEmote(player.getId(), cap.getEmoteData()));
-						break;
-					}
+                    if (Minecraft.getInstance().player != null && player.getId() == Minecraft.getInstance().player.getId()) {
+                        if (player.isCrouching() || player.swinging) {
+                            EmoteMenuHandler.clearEmotes(player);
+                            return;
+                        }
 
-					if(Minecraft.getInstance().player != null && player.getId() == Minecraft.getInstance().player.getId()){
-						if(player.isCrouching() || player.swinging){
-							EmoteMenuHandler.clearEmotes(player);
-							return;
-						}
+                        if (emote.thirdPerson) {
+                            Minecraft.getInstance().levelRenderer.needsUpdate();
+                            CameraType pointofview = Minecraft.getInstance().options.getCameraType();
 
-						if(emote.thirdPerson){
-							Minecraft.getInstance().levelRenderer.needsUpdate();
-							CameraType pointofview = Minecraft.getInstance().options.getCameraType();
+                            if (pointofview.isFirstPerson()) {
+                                Minecraft.getInstance().options.setCameraType(THIRD_PERSON_BACK);
 
-							if(pointofview.isFirstPerson()){
-								Minecraft.getInstance().options.setCameraType(THIRD_PERSON_BACK);
+                                if (pointofview.isFirstPerson() != Minecraft.getInstance().options.getCameraType().isFirstPerson()) {
+                                    Minecraft.getInstance().gameRenderer.checkEntityPostEffect(Minecraft.getInstance().options.getCameraType().isFirstPerson() ? Minecraft.getInstance().getCameraEntity() : null);
+                                }
+                            }
+                        }
+                    }
 
-								if(pointofview.isFirstPerson() != Minecraft.getInstance().options.getCameraType().isFirstPerson()){
-									Minecraft.getInstance().gameRenderer.checkEntityPostEffect(Minecraft.getInstance().options.getCameraType().isFirstPerson() ? Minecraft.getInstance().getCameraEntity() : null);
-								}
-							}
-						}
-					}
+                    if (Arrays.stream(cap.getEmoteData().currentEmotes).anyMatch(Objects::nonNull)) {
+                        if (emote.sound != null && emote.sound.interval > 0) {
+                            if (cap.getEmoteData().emoteTicks[index] % emote.sound.interval == 0) {
+                                player.level().playLocalSound(player.position().x, player.position().y, player.position().z, SoundEvent.createVariableRangeEvent(new ResourceLocation(emote.sound.key)), SoundSource.PLAYERS, emote.sound.volume, emote.sound.pitch, false);
+                            }
+                        }
 
-					if(Arrays.stream(cap.getEmoteData().currentEmotes).anyMatch(Objects::nonNull)){
-						if(emote.sound != null && emote.sound.interval > 0){
-							if(cap.getEmoteData().emoteTicks[index] % emote.sound.interval == 0){
-								player.level().playLocalSound(player.position().x, player.position().y, player.position().z, SoundEvent.createVariableRangeEvent(new ResourceLocation(emote.sound.key)), SoundSource.PLAYERS, emote.sound.volume, emote.sound.pitch, false);
-							}
-						}
+                        if (ServerConfig.canMoveInEmote) {
+                            if (emote.animation != null && !emote.animation.isEmpty()) {
+                                AttributeInstance attributeInstance = player.getAttribute(Attributes.MOVEMENT_SPEED);
+                                AttributeModifier noMove = new AttributeModifier(EMOTE_NO_MOVE, "EMOTE", -attributeInstance.getValue(), AttributeModifier.Operation.ADDITION);
 
-						if(ServerConfig.canMoveInEmote){
-							if(emote.animation != null && !emote.animation.isEmpty()){
-								AttributeInstance attributeInstance = player.getAttribute(Attributes.MOVEMENT_SPEED);
-								AttributeModifier noMove = new AttributeModifier(EMOTE_NO_MOVE, "EMOTE", -attributeInstance.getValue(), AttributeModifier.Operation.ADDITION);
+                                if (!attributeInstance.hasModifier(noMove)) {
+                                    attributeInstance.addTransientModifier(noMove);
+                                }
+                            }
+                        }
+                    }
+                }
 
-								if(!attributeInstance.hasModifier(noMove)){
-									attributeInstance.addTransientModifier(noMove);
-								}
-							}
-						}
-					}
-				}
+                if (Arrays.stream(cap.getEmoteData().currentEmotes).noneMatch(Objects::nonNull) && ServerConfig.canMoveInEmote) {
+                    AttributeInstance attributeInstance = player.getAttribute(Attributes.MOVEMENT_SPEED);
+                    AttributeModifier noMove = new AttributeModifier(EMOTE_NO_MOVE, "EMOTE", -attributeInstance.getValue(), AttributeModifier.Operation.ADDITION);
 
-				if(Arrays.stream(cap.getEmoteData().currentEmotes).noneMatch(Objects::nonNull) && ServerConfig.canMoveInEmote){
-					AttributeInstance attributeInstance = player.getAttribute(Attributes.MOVEMENT_SPEED);
-					AttributeModifier noMove = new AttributeModifier(EMOTE_NO_MOVE, "EMOTE", -attributeInstance.getValue(), AttributeModifier.Operation.ADDITION);
+                    if (attributeInstance.hasModifier(noMove)) {
+                        attributeInstance.removeModifier(EMOTE_NO_MOVE);
+                    }
+                }
+            });
+        }
+    }
 
-					if(attributeInstance.hasModifier(noMove)){
-						attributeInstance.removeModifier(EMOTE_NO_MOVE);
-					}
-				}
-			});
-		}
-	}
+    @SubscribeEvent
+    public static void playerAttacked(final LivingHurtEvent event) {
+        EmoteMenuHandler.clearEmotes(event.getEntity());
+    }
 
-	@SubscribeEvent
-	public static void playerAttacked(LivingHurtEvent event){
-		EmoteMenuHandler.clearEmotes(event.getEntity());
-	}
+    @SubscribeEvent
+    public static void playerTick(final ClientTickEvent event) {
+        if (event.phase == Phase.START) {
+            return;
+        }
 
-	@SubscribeEvent
-	public static void playerTick(ClientTickEvent event){
-		if(event.phase == Phase.START){
-			return;
-		}
-
-		DragonStateProvider.getCap(Minecraft.getInstance().player).ifPresent(cap -> {
-			if(Arrays.stream(cap.getEmoteData().currentEmotes).anyMatch(Objects::nonNull)){
-				for(int index = 0; index < cap.getEmoteData().currentEmotes.length; index++){
-					Emote emote = cap.getEmoteData().currentEmotes[index];
-					if(emote != null && !emote.loops){
-						if(cap.getEmoteData().emoteTicks[index] >= emote.duration){
-							cap.getEmoteData().currentEmotes[index] = null;
-							cap.getEmoteData().emoteTicks[index] = 0;
-							NetworkHandler.CHANNEL.sendToServer(new SyncEmote(Minecraft.getInstance().player.getId(), cap.getEmoteData()));
-						}
-					}
-				}
-			}
-		});
-	}
+        DragonStateProvider.getCap(Minecraft.getInstance().player).ifPresent(cap -> {
+            if (Arrays.stream(cap.getEmoteData().currentEmotes).anyMatch(Objects::nonNull)) {
+                for (int index = 0; index < cap.getEmoteData().currentEmotes.length; index++) {
+                    Emote emote = cap.getEmoteData().currentEmotes[index];
+                    if (emote != null && !emote.loops) {
+                        if (cap.getEmoteData().emoteTicks[index] >= emote.duration) {
+                            cap.getEmoteData().currentEmotes[index] = null;
+                            cap.getEmoteData().emoteTicks[index] = 0;
+                            NetworkHandler.CHANNEL.sendToServer(new SyncEmote(Minecraft.getInstance().player.getId(), cap.getEmoteData()));
+                        }
+                    }
+                }
+            }
+        });
+    }
 }
