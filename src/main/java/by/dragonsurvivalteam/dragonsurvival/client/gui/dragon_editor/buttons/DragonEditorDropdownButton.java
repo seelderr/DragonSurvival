264e41680c19ea27145397ed83192d7d92190521
Seelder <zlschect@gmail.com>
2024-05-20 19:16:06 -0700

    Fixed editor dropdown button

--- src/main/java/by/dragonsurvivalteam/dragonsurvival/client/gui/dragon_editor/buttons/DragonEditorDropdownButton.java
+++ src/main/java/by/dragonsurvivalteam/dragonsurvival/client/gui/dragon_editor/buttons/DragonEditorDropdownButton.java
@@ -3,7 +3,6 @@ package by.dragonsurvivalteam.dragonsurvival.client.gui.dragon_editor.buttons;
 import by.dragonsurvivalteam.dragonsurvival.client.gui.dragon_editor.DragonEditorScreen;
 import by.dragonsurvivalteam.dragonsurvival.client.gui.widgets.buttons.dropdown.DropdownEntry;
 import by.dragonsurvivalteam.dragonsurvival.client.gui.widgets.buttons.dropdown.DropdownList;
-import by.dragonsurvivalteam.dragonsurvival.client.gui.widgets.buttons.dropdown.DropdownValueEntry;
 import by.dragonsurvivalteam.dragonsurvival.client.gui.widgets.buttons.generic.DropDownButton;
 import by.dragonsurvivalteam.dragonsurvival.client.skin_editor_system.DragonEditorHandler;
 import by.dragonsurvivalteam.dragonsurvival.client.skin_editor_system.EnumSkinLayer;
@@ -13,14 +12,15 @@ import com.mojang.blaze3d.systems.RenderSystem;
 import com.mojang.blaze3d.vertex.PoseStack;
 
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.client.gui.components.ContainerObjectSelectionList;
 import net.minecraft.client.gui.components.events.GuiEventListener;
 import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.network.chat.Component;
 import net.minecraftforge.client.gui.widget.ExtendedButton;
+import org.jetbrains.annotations.NotNull;
 
-import java.util.ArrayList;
-import java.util.Locale;
+import java.util.List;
 import java.util.Objects;
 
 public class DragonEditorDropdownButton extends DropDownButton{
@@ -74,8 +74,8 @@ public class DragonEditorDropdownButton extends DropDownButton{
 		Screen screen = Minecraft.getInstance().screen;
 
 		if(!toggled){
-			int offset = screen.height - (y + height + 80);
-			list = new DropdownList(x, y + height + Math.min(offset, 0), width, (int)(Math.max(1, Math.min(values.length, maxItems)) * (height * 1.5f)), 16);
+			int offset = screen.height - (getY() + height + 80);
+			list = new DropdownList(getX(), getY() + height + Math.min(offset, 0), width, (int)(Math.max(1, Math.min(values.length, maxItems)) * (height * 1.5f)), 16);
 			DropdownEntry center = null;
 
 			for(int i = 0; i < values.length; i++){
@@ -113,7 +113,7 @@ public class DragonEditorDropdownButton extends DropDownButton{
 			boolean finalHasBorder = hasBorder;
 			renderButton = new ExtendedButton(0, 0, 0, 0, Component.empty(), null){
 				@Override
-				public void render(PoseStack p_230430_1_, int p_230430_2_, int p_230430_3_, float p_230430_4_){
+				public void renderWidget(GuiGraphics graphics, int mouseX, int mouseY, float partialTick){
 					active = visible = false;
 					list.visible = DragonEditorDropdownButton.this.visible;
 
@@ -121,7 +121,7 @@ public class DragonEditorDropdownButton extends DropDownButton{
 						RenderSystem.enableScissor(0, (int)(32 * Minecraft.getInstance().getWindow().getGuiScale()), Minecraft.getInstance().getWindow().getScreenWidth(), Minecraft.getInstance().getWindow().getScreenHeight() - (int)(32 * Minecraft.getInstance().getWindow().getGuiScale()) * 2);
 
 					if(list.visible)
-						list.render(p_230430_1_, p_230430_2_, p_230430_3_, p_230430_4_);
+						list.render(graphics, mouseX, mouseY, partialTick);
 
 					if(finalHasBorder)
 						RenderSystem.disableScissor();
@@ -144,84 +144,3 @@ public class DragonEditorDropdownButton extends DropDownButton{
 		return new DragonDropdownValueEntry(this, pos, val, localeString, setter);
 	}
 }
-/*	@Override
-	public void onPress(){
-		Screen screen = Minecraft.getInstance().screen;
-
-		if(!toggled){
-			int offset = screen.height - (getY() + height + 80);
-			list = new DropdownList(getX(), getY() + height + Math.min(offset, 0), width, (int)(Math.max(1, Math.min(values.length, maxItems)) * (height * 1.5f)), width / 2 - 6);
-			DropdownEntry center = null;
-
-			for(int i = 0; i < values.length; i += 2){
-				String val = values[i];
-				String val2 = i < values.length - 1 ? values[i + 1] : null;
-
-				DragonEditorDropdownEntry ent = new DragonEditorDropdownEntry(this, i);
-
-				int width = list.getWidth() / 2 - 6;
-
-				ent.children.add(new EditorPartButton(dragonEditorScreen, this, getX() + 3, getY(), width, width, val, setter, layers));
-
-				if(val2 != null){
-					ent.children.add(new EditorPartButton(dragonEditorScreen, this, getX() + 3 + width, getY(), width, width, val2, setter, layers));
-				}
-
-				list.addEntry(ent);
-
-				if(Objects.equals(val, current)){
-					center = ent;
-				}
-			}
-
-			if(center != null){
-				list.centerScrollOn(center);
-			}
-
-			boolean hasBorder = false;
-			if(!screen.children.isEmpty()){
-				screen.children.add(0, list);
-				screen.children.add(list);
-
-				for(GuiEventListener child : screen.children){
-					if(child instanceof AbstractSelectionList){
-						if(((AbstractSelectionList<?>)child).renderTopAndBottom){
-							hasBorder = true;
-							break;
-						}
-					}
-				}
-			}else{
-				screen.children.add(list);
-			}
-
-			boolean finalHasBorder = hasBorder;
-			renderButton = new ExtendedButton(0, 0, 0, 0, Component.empty(), null){
-				@Override
-				public void render(@NotNull final GuiGraphics guiGraphics, int p_230430_2_, int p_230430_3_, float p_230430_4_){
-					active = visible = false;
-					list.visible = DragonEditorDropdownButton.this.visible;
-
-					if(finalHasBorder){
-						RenderSystem.enableScissor(0, (int)(32 * Minecraft.getInstance().getWindow().getGuiScale()), Minecraft.getInstance().getWindow().getScreenWidth(), Minecraft.getInstance().getWindow().getScreenHeight() - (int)(32 * Minecraft.getInstance().getWindow().getGuiScale()) * 2);
-					}
-
-					if(list.visible){
-						// TODO :: What does this render exactly? Disabling it doesn't seem to change anything
-						list.render(guiGraphics, p_230430_2_, p_230430_3_, p_230430_4_);
-					}
-
-					if(finalHasBorder){
-						RenderSystem.disableScissor();
-					}
-				}
-			};
-			screen.renderables.add(renderButton);
-		}else{
-			screen.children.removeIf(s -> s == list);
-			screen.renderables.removeIf(s -> s == renderButton);
-		}
-
-		toggled = !toggled;
-		updateMessage();
-	}*/
