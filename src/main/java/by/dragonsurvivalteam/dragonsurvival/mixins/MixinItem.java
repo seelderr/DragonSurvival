ab00fbf9247757e3acd77da3367437651de38667
Seelder <zlschect@gmail.com>
2024-05-27 15:01:59 -0700

    Fixed a compatibility issue with FarmersDelight (and probably other food mods)

--- src/main/java/by/dragonsurvivalteam/dragonsurvival/mixins/MixinItem.java
+++ src/main/java/by/dragonsurvivalteam/dragonsurvival/mixins/MixinItem.java
@@ -1,34 +1,56 @@
 package by.dragonsurvivalteam.dragonsurvival.mixins;
 
+import by.dragonsurvivalteam.dragonsurvival.common.capability.DragonStateHandler;
 import by.dragonsurvivalteam.dragonsurvival.common.capability.DragonStateProvider;
 import by.dragonsurvivalteam.dragonsurvival.common.handlers.DragonFoodHandler;
 import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
 import net.minecraft.world.InteractionHand;
-import net.minecraft.world.InteractionResultHolder;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.food.FoodProperties;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin( Item.class )
 public class MixinItem{
+	@Unique
+	private boolean dragon_Survival$isEdible(boolean original, Player player) {
+		DragonStateHandler handler = DragonStateProvider.getHandler(player);
+		if(handler != null && handler.isDragon()){
+			return DragonFoodHandler.isDragonEdible((Item)(Object)this, handler.getType());
+		}
+
+		return original;
+	}
+
 	@ModifyExpressionValue (method = "use", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/item/ItemStack;isEdible()Z"))
 	private boolean dragonIsEdible(boolean original, Level level, Player player, InteractionHand hand){
-		return DragonStateProvider.getCap(player).map(dragonStateHandler -> !dragonStateHandler.isDragon() || DragonFoodHandler.isDragonEdible((Item)(Object)this, dragonStateHandler.getType())).orElse(original);
+		return dragon_Survival$isEdible(original, player);
 	}
 
 	@ModifyExpressionValue (method ="use", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/player/Player;canEat(Z)Z"))
 	private boolean dragonCanEat(boolean original, Level level, Player player, InteractionHand hand){
-		return DragonStateProvider.getCap(player).map(dragonStateHandler -> !dragonStateHandler.isDragon() || player.canEat(DragonFoodHandler.getDragonFoodProperties((Item)(Object)this, dragonStateHandler.getType()).canAlwaysEat())).orElse(original);
+		DragonStateHandler handler = DragonStateProvider.getHandler(player);
+		if(handler != null && handler.isDragon()){
+			FoodProperties dragonFoodProperties = DragonFoodHandler.getDragonFoodProperties((Item)(Object)this, handler.getType());
+			if(dragonFoodProperties != null) {
+				return player.canEat(dragonFoodProperties.canAlwaysEat());
+			}
+		}
+
+		return original;
 	}
 
 	@ModifyExpressionValue (method = "finishUsingItem", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/item/Item;isEdible()Z"))
 	private boolean dragonFinishUsingItem(boolean original, ItemStack item, Level level, LivingEntity player){
-		return DragonStateProvider.getCap(player).map(dragonStateHandler -> !dragonStateHandler.isDragon() || DragonFoodHandler.isDragonEdible((Item)(Object)this, dragonStateHandler.getType())).orElse(original);
+		if (player instanceof Player) {
+			return dragon_Survival$isEdible(original, (Player) player);
+		}
+
+		return original;
 	}
 }
