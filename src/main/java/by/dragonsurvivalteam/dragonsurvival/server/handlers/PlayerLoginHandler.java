ebbf6290a73eea0f1c444cbac767027bea15e58a
Seelder <zlschect@gmail.com>
2024-06-12 02:05:08 -0700

    Fixed an issue where you could attempt to load skin data before the data is actually ready

--- src/main/java/by/dragonsurvivalteam/dragonsurvival/server/handlers/PlayerLoginHandler.java
+++ src/main/java/by/dragonsurvivalteam/dragonsurvival/server/handlers/PlayerLoginHandler.java
@@ -2,20 +2,42 @@ package by.dragonsurvivalteam.dragonsurvival.server.handlers;
 
 import by.dragonsurvivalteam.dragonsurvival.common.capability.DragonStateProvider;
 import by.dragonsurvivalteam.dragonsurvival.network.RequestClientData;
+import com.mojang.datafixers.util.Pair;
 import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.entity.player.Player;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.neoforge.event.entity.player.PlayerEvent;
+import net.neoforged.neoforge.event.tick.PlayerTickEvent;
 import net.neoforged.neoforge.network.PacketDistributor;
+import org.apache.commons.lang3.tuple.MutablePair;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @EventBusSubscriber(bus = EventBusSubscriber.Bus.GAME)
 public class PlayerLoginHandler {
-    // We don't need to sync the player capability data anymore on login, but we do need to request skin data.
+    static List<MutablePair<ServerPlayer, Integer>> loginDataRequestTimings = new ArrayList<MutablePair<ServerPlayer, Integer>>();
+
+    // We need to delay the syncing of player data a little bit, since if we let the player sync too fast then it is possible for the player
+    // to attempt to sync data whilst their data is still being loaded, causing the sync to fail.
     @SubscribeEvent
     public static void onPlayerLoggedIn(final PlayerEvent.PlayerLoggedInEvent event) {
-        if (event.getEntity() instanceof ServerPlayer serverPlayer) {
-            DragonStateProvider.getCap(serverPlayer).ifPresent(handler -> {
-                PacketDistributor.sendToPlayer(serverPlayer, new RequestClientData.Data(handler.getType(), handler.getBody(), handler.getLevel()));
+        loginDataRequestTimings.add(MutablePair.of((ServerPlayer)event.getEntity(), 10));
+    }
+
+    @SubscribeEvent
+    public static void onPlayerTick(final PlayerTickEvent.Pre event) {
+        if(!loginDataRequestTimings.isEmpty()) {
+            loginDataRequestTimings.removeIf(pair -> {
+                if (pair.getRight() <= 0) {
+                    DragonStateProvider.getCap(pair.getLeft()).ifPresent(handler -> {
+                        PacketDistributor.sendToPlayer(pair.getLeft(), new RequestClientData.Data(handler.getType(), handler.getBody(), handler.getLevel()));
+                    });
+                    return true;
+                }
+                pair.setRight(pair.getRight() - 1);
+                return false;
             });
         }
     }
