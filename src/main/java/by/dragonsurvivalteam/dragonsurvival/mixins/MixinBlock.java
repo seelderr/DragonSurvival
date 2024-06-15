89698cb93308876da54f4dfd6ad38606afed473e
Seelder <zlschect@gmail.com>
2024-06-15 12:37:28 -0700

    Fixed cave dragon drop protection not working correctly

--- src/main/java/by/dragonsurvivalteam/dragonsurvival/mixins/MixinBlock.java
+++ src/main/java/by/dragonsurvivalteam/dragonsurvival/mixins/MixinBlock.java
@@ -15,11 +15,11 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import javax.annotation.Nullable;
-import javax.security.auth.callback.Callback;
 import java.util.Stack;
 import java.util.function.Supplier;
 
@@ -35,10 +35,10 @@ public class MixinBlock {
 		}
 	}
 
-	@ModifyVariable(method = "popResource(Lnet/minecraft/world/level/Level;Ljava/util/function/Supplier;Lnet/minecraft/world/item/ItemStack;)V", at = @At(value = "STORE"), argsOnly = true)
-	private static ItemEntity makeDropsSafe(Level pLevel, Supplier<ItemEntity> pItemEntitySupplier, ItemStack pStack) {
+	@ModifyArg(method = "popResource(Lnet/minecraft/world/level/Level;Ljava/util/function/Supplier;Lnet/minecraft/world/item/ItemStack;)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/Level;addFreshEntity(Lnet/minecraft/world/entity/Entity;)Z"))
+	private static Entity makeDropsSafe(Entity originalEntity) {
 		if (!dragon_Survival$fireImmuneItems.isEmpty() && dragon_Survival$fireImmuneItems.pop()) {
-			ItemEntity itemEntity = pItemEntitySupplier.get();
+			ItemEntity itemEntity = (ItemEntity) originalEntity;
 			return new ItemEntity(itemEntity.level, itemEntity.getX(), itemEntity.getY(), itemEntity.getZ(), itemEntity.getItem()) {
 				@Override
 				public boolean fireImmune() {
@@ -47,6 +47,6 @@ public class MixinBlock {
 			};
 		}
 
-		return pItemEntitySupplier.get();
+		return originalEntity;
 	}
 }
