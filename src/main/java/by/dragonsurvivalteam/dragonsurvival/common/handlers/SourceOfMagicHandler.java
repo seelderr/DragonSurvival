1fd903a414efe0fd7edc7b9f7baa011cf2ad7fd4
Seelder <zlschect@gmail.com>
2024-06-20 21:11:50 -0700

    SourceOfMagic is now functional

--- src/main/java/by/dragonsurvivalteam/dragonsurvival/common/handlers/SourceOfMagicHandler.java
+++ src/main/java/by/dragonsurvivalteam/dragonsurvival/common/handlers/SourceOfMagicHandler.java
@@ -12,7 +12,10 @@ import by.dragonsurvivalteam.dragonsurvival.server.tileentity.SourceOfMagicPlace
 import by.dragonsurvivalteam.dragonsurvival.server.tileentity.SourceOfMagicTileEntity;
 import by.dragonsurvivalteam.dragonsurvival.util.Functions;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.resources.sounds.Sound;
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.sounds.SoundSource;
 import net.minecraft.util.Mth;
 import net.minecraft.util.RandomSource;
 import net.minecraft.world.effect.MobEffectInstance;
@@ -26,13 +29,35 @@ import net.neoforged.api.distmarker.OnlyIn;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.neoforge.client.event.ClientTickEvent;
+import net.neoforged.neoforge.common.data.SoundDefinition;
 import net.neoforged.neoforge.event.entity.living.LivingHurtEvent;
 import net.neoforged.neoforge.event.tick.PlayerTickEvent;
 import net.neoforged.neoforge.network.PacketDistributor;
 
 
 @EventBusSubscriber
-public class SourceOfMagicHandler{
+public class SourceOfMagicHandler {
+
+	public static void cancelSourceOfMagicServer(Player player){
+		DragonStateProvider.getCap(player).ifPresent(cap -> {
+			if(cap.getMagicData().onMagicSource){
+				cap.getMagicData().onMagicSource = false;
+				cap.getMagicData().magicSourceTimer = 0;
+				PacketDistributor.sendToPlayersTrackingEntityAndSelf(player, new SyncMagicSourceStatus.Data(player.getId(), false, 0));
+			}
+		});
+	}
+
+	public static void cancelSourceOfMagicClient(Player player){
+		DragonStateProvider.getCap(player).ifPresent(cap -> {
+			if(cap.getMagicData().onMagicSource){
+				cap.getMagicData().onMagicSource = false;
+				cap.getMagicData().magicSourceTimer = 0;
+				PacketDistributor.sendToServer(new SyncMagicSourceStatus.Data(player.getId(), false, 0));
+			}
+		});
+	}
+
 	@SubscribeEvent
 	public static void playerTick(PlayerTickEvent.Pre event){
 		if(event.getEntity().level().isClientSide()){
@@ -45,10 +70,8 @@ public class SourceOfMagicHandler{
 			DragonStateHandler handler = DragonStateProvider.getOrGenerateHandler(player);
 
 			if(handler.getMagicData().onMagicSource){
-				if(!(player.getBlockStateOn().getBlock() instanceof SourceOfMagicBlock) || handler.getMovementData().bite || player.isCrouching() && handler.getMagicData().magicSourceTimer > 40){
-					handler.getMagicData().onMagicSource = false;
-					handler.getMagicData().magicSourceTimer = 0;
-					PacketDistributor.sendToPlayersTrackingEntityAndSelf(player, new SyncMagicSourceStatus.Data(player.getId(), false, 0));
+				if(!(player.getBlockStateOn().getBlock() instanceof SourceOfMagicBlock)){
+					cancelSourceOfMagicServer(player);
 					return;
 				}
 
@@ -65,86 +88,46 @@ public class SourceOfMagicHandler{
 					if(!tile.isEmpty()){
 						BlockState pState = sourceOfMagic.getBlockState();
 						
-						if(!SourceOfMagicBlock.shouldHarmPlayer(pState, player) || player.isCreative()){
+						if(!SourceOfMagicBlock.shouldHarmPlayer(pState, player)){
 								if(handler.getMagicData().magicSourceTimer >= Functions.secondsToTicks(10)){
 									handler.getMagicData().magicSourceTimer = 0;
 									MobEffectInstance effectInstance = player.getEffect(DSEffects.SOURCE_OF_MAGIC);
 									int duration = SourceOfMagicTileEntity.consumables.get(tile.getItem(0).getItem());
 
 									if(effectInstance == null){
-										player.addEffect(new MobEffectInstance(DSEffects.SOURCE_OF_MAGIC, duration));
+										player.addEffect(new MobEffectInstance(DSEffects.SOURCE_OF_MAGIC, duration, 0, true, false));
 									}else{
-										player.addEffect(new MobEffectInstance(DSEffects.SOURCE_OF_MAGIC, effectInstance.getDuration() + duration));
+										player.addEffect(new MobEffectInstance(DSEffects.SOURCE_OF_MAGIC, effectInstance.getDuration() + duration, 0, true, false));
 									}
 
 									tile.removeItem(0, 1);
+								} else {
+									RandomSource random = player.getRandom();
+									double x = -1 + random.nextDouble() * 2;
+									double z = -1 + random.nextDouble() * 2;
+
+									if(pState.getBlock() == DSBlocks.SEA_SOURCE_OF_MAGIC.get() || pState.getBlock() == DSBlocks.FOREST_SOURCE_OF_MAGIC.get()){
+										player.level().addParticle(new BeaconParticle.MagicData(), player.getX() + x, player.getY() + 0.5, player.getZ() + z, 0, 0, 0);
+									}else if(pState.getBlock() == DSBlocks.CAVE_SOURCE_OF_MAGIC.get()){
+										player.level().addParticle(new BeaconParticle.FireData(), player.getX() + x, player.getY() + 0.5, player.getZ() + z, 0, 0, 0);
+									}
+
+									handler.getMagicData().magicSourceTimer++;
 								}
 							}
 						}else{
-							if(ServerConfig.damageWrongSourceOfMagic){
+							if(ServerConfig.damageWrongSourceOfMagic && !player.isCreative()){
 								if(player.tickCount % Functions.secondsToTicks(5) == 0){
 									player.hurt(player.damageSources().magic(), 1F);
 								}
 							}
 						}
 					}else{
-						handler.getMagicData().magicSourceTimer = 0;
-						handler.getMagicData().onMagicSource = false;
-						PacketDistributor.sendToPlayersTrackingEntityAndSelf(player, new SyncMagicSourceStatus.Data(player.getId(), false, 0));
+						cancelSourceOfMagicServer(player);
 					}
 				}
 			}
 		}
-
-	@SubscribeEvent
-	public static void playerParticles(PlayerTickEvent.Post event){
-		if(event.getEntity().level().isClientSide()){
-			return;
-		}
-
-		Player player = event.getEntity();
-
-		if(DragonStateProvider.isDragon(player)){
-			DragonStateHandler handler = DragonStateProvider.getOrGenerateHandler(player);
-
-			if(handler.getMagicData().onMagicSource){
-				BlockPos pos1 = player.blockPosition();
-				BlockEntity blockEntity = player.level().getBlockEntity(pos1);
-
-				if(blockEntity instanceof SourceOfMagicPlaceholder){
-					pos1 = ((SourceOfMagicPlaceholder)blockEntity).rootPos;
-				}
-
-				BlockEntity sourceOfMagic = player.level().getBlockEntity(pos1);
-
-				if(sourceOfMagic instanceof SourceOfMagicTileEntity tile){
-
-					if(!tile.isEmpty()){
-						BlockState pState = sourceOfMagic.getBlockState();
-						
-						if(!SourceOfMagicBlock.shouldHarmPlayer(pState, player) || player.isCreative()){
-								if(player.level().isClientSide()){
-									Minecraft minecraft = Minecraft.getInstance();
-									RandomSource random = player.getRandom();
-									double x = -1 + random.nextDouble() * 2;
-									double z = -1 + random.nextDouble() * 2;
-
-									if(pState.getBlock() == DSBlocks.SEA_SOURCE_OF_MAGIC.get() || pState.getBlock() == DSBlocks.FOREST_SOURCE_OF_MAGIC.get()){
-										if(!minecraft.isPaused()){
-											player.level().addParticle(new BeaconParticle.MagicData(), player.getX() + x, player.getY() + 0.5, player.getZ() + z, 0, 0, 0);
-										}
-									}else if(pState.getBlock() == DSBlocks.CAVE_SOURCE_OF_MAGIC.get()){
-										if(!minecraft.isPaused()){
-											player.level().addParticle(new BeaconParticle.FireData(), player.getX() + x, player.getY() + 0.5, player.getZ() + z, 0, 0, 0);
-										}
-									}
-								}
-						}
-					}
-				}
-			}
-		}
-	}
 
 	@OnlyIn( Dist.CLIENT )
 	@SubscribeEvent
@@ -157,8 +140,8 @@ public class SourceOfMagicHandler{
 			if(handler.getMagicData().onMagicSource){
 				Vec3 velocity = player.getDeltaMovement();
 				float groundSpeed = Mth.sqrt((float)(velocity.x * velocity.x + velocity.z * velocity.z));
-				if(Math.abs(groundSpeed) > 0.05){
-					PacketDistributor.sendToServer(new SyncMagicSourceStatus.Data(player.getId(), false, 0));
+				if(Math.abs(groundSpeed) > 0.05 || handler.getMovementData().dig || player.isCrouching() && handler.getMagicData().magicSourceTimer > 40){
+					cancelSourceOfMagicClient(player);
 				}
 			}
 		}
@@ -172,12 +155,12 @@ public class SourceOfMagicHandler{
 			if(!player.level().isClientSide()){
 				DragonStateProvider.getCap(player).ifPresent(cap -> {
 					if(cap.getMagicData().onMagicSource){
-						cap.getMagicData().onMagicSource = false;
-						cap.getMagicData().magicSourceTimer = 0;
-						PacketDistributor.sendToPlayersTrackingEntityAndSelf(player, new SyncMagicSourceStatus.Data(player.getId(), false, 0));
+						cancelSourceOfMagicServer(player);
 					}
 				});
 			}
 		}
 	}
+
+	// There is a third case for mining as well. See MiningTickHandler.java
 }
