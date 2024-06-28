d14e945754ea62bff92764d84b6e661d2cba84ea
Psither <97207971+Psither@users.noreply.github.com>
2024-06-28 04:33:39 -0400

    Dragonsbane

---
+++ src/main/java/by/dragonsurvivalteam/dragonsurvival/registry/DSEnchantments.java
@@ -0,0 +1,17 @@
+package by.dragonsurvivalteam.dragonsurvival.registry;
+
+import net.minecraft.core.registries.Registries;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.enchantment.Enchantment;
+
+import static by.dragonsurvivalteam.dragonsurvival.DragonSurvivalMod.MODID;
+
+public class DSEnchantments {
+    private static ResourceKey<Enchantment> register(String key) {
+        return ResourceKey.create(Registries.ENCHANTMENT, ResourceLocation.fromNamespaceAndPath(MODID, key));
+    }
+
+    public static final ResourceKey<Enchantment> DRAGONSBANE = register("dragonsbane");
+}
+
