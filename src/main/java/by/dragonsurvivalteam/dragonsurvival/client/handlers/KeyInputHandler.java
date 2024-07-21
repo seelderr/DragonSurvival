b5a46893460a40196743189c182300c157d6ac2c
Metaflame <metaflamedragon@gmail.com>
2024-07-21 02:37:17 +0200

    Move dragon inventory toggle

--- src/main/java/by/dragonsurvivalteam/dragonsurvival/client/handlers/KeyInputHandler.java
+++ src/main/java/by/dragonsurvivalteam/dragonsurvival/client/handlers/KeyInputHandler.java
@@ -16,24 +16,4 @@ import net.neoforged.neoforge.network.PacketDistributor;
 
 @EventBusSubscriber(Dist.CLIENT)
 public class KeyInputHandler {
-
-    @SubscribeEvent
-    public static void onKey(InputEvent.Key keyInputEvent) {
-        Minecraft minecraft = Minecraft.getInstance();
-        LocalPlayer player = Minecraft.getInstance().player;
-
-        if (player == null || !DragonStateProvider.isDragon(minecraft.player))
-            return;
-
-        DragonStateHandler dragonStateHandler = DragonStateProvider.getOrGenerateHandler(player);
-
-        if (Keybind.DRAGON_INVENTORY.consumeClick()) {
-            if (minecraft.screen == null) {
-                PacketDistributor.sendToServer(new RequestOpenDragonInventory.Data());
-            } else {
-                player.closeContainer();
-            }
-
-        }
-    }
 }
