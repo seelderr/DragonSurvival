9fc1b7203cc9f900b134366de5afb9eb9725c6be
BlackAures1 <dragonsurvival@mail.ru>
2024-08-03 23:59:06 +0300

    - wip structure - wip vault

--- src/main/java/by/dragonsurvivalteam/dragonsurvival/server/handlers/BlockEntityHandler.java
+++ src/main/java/by/dragonsurvivalteam/dragonsurvival/server/handlers/BlockEntityHandler.java
@@ -11,8 +11,8 @@ import net.neoforged.neoforge.event.BlockEntityTypeAddBlocksEvent;
 public class BlockEntityHandler {
     @SubscribeEvent
     public static void addToBlockEntityType(BlockEntityTypeAddBlocksEvent e) {
-        e.modify(BlockEntityType.VAULT, DSBlocks.GOOD_DRAGON_VAULT.get());
-        e.modify(BlockEntityType.VAULT, DSBlocks.EVIL_DRAGON_VAULT.get());
-        e.modify(BlockEntityType.VAULT, DSBlocks.HUNTER_DRAGON_VAULT.get());
+        e.modify(BlockEntityType.VAULT, DSBlocks.DRAGON_VAULT_FRIENDLY.get());
+        e.modify(BlockEntityType.VAULT, DSBlocks.DRAGON_VAULT_ANGRY.get());
+        e.modify(BlockEntityType.VAULT, DSBlocks.DRAGON_VAULT_HUNTER.get());
     }
 }
