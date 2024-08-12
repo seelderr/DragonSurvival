89791adb537663ccf6669160b83ab18d2b015bcf
Psither <97207971+Psither@users.noreply.github.com>
2024-08-12 00:25:46 -0400

    codec

--- src/main/java/by/dragonsurvivalteam/dragonsurvival/registry/DSDataComponents.java
+++ src/main/java/by/dragonsurvivalteam/dragonsurvival/registry/DSDataComponents.java
@@ -5,12 +5,13 @@ import java.util.function.UnaryOperator;
 import net.minecraft.core.Registry;
 import net.minecraft.core.component.DataComponentType;
 import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.network.codec.ByteBufCodecs;
 import net.minecraft.util.ExtraCodecs;
 import org.joml.Vector3f;
 
 public class DSDataComponents {
     //public static DataComponentType<ExtraCodecs.TagOrElementLocation> VALID_VAULTS = register("valid_vaults", e -> e.persistent(ExtraCodecs.TAG_OR_ELEMENT_ID));
-    public static DataComponentType<Vector3f> TARGET_POSITION = register("target_position", e -> e.persistent(ExtraCodecs.VECTOR3F));
+    public static DataComponentType<Vector3f> TARGET_POSITION = register("target_position", e -> e.persistent(ExtraCodecs.VECTOR3F).networkSynchronized(ByteBufCodecs.VECTOR3F));
 
     private static <T> DataComponentType<T> register(String pName, UnaryOperator<DataComponentType.Builder<T>> pBuilder) {
         return Registry.register(BuiltInRegistries.DATA_COMPONENT_TYPE, pName, pBuilder.apply(DataComponentType.builder()).build());
