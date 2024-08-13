ef9aadf25db78377d1e2368d442c659688c597bf
Kai <kai.zahn@yahoo.de>
2024-08-13 10:46:21 +0200

    indent

--- src/main/java/by/dragonsurvivalteam/dragonsurvival/util/CompoundTagBuilder.java
+++ src/main/java/by/dragonsurvivalteam/dragonsurvival/util/CompoundTagBuilder.java
@@ -4,28 +4,28 @@ import net.minecraft.nbt.CompoundTag;
 import net.minecraft.nbt.Tag;
 
 public class CompoundTagBuilder {
-		private final CompoundTag tag = new CompoundTag();
+    private final CompoundTag tag = new CompoundTag();
 
-		public static CompoundTagBuilder tag() {
-			return new CompoundTagBuilder();
-		}
+    public static CompoundTagBuilder tag() {
+        return new CompoundTagBuilder();
+    }
 
-		public CompoundTagBuilder putTag(final String key, final Tag value) {
-			tag.put(key, value);
-			return this;
-		}
+    public CompoundTagBuilder putTag(final String key, final Tag value) {
+        tag.put(key, value);
+        return this;
+    }
 
-		public CompoundTagBuilder putString(final String key, final String value) {
-			tag.putString(key, value);
-			return this;
-		}
+    public CompoundTagBuilder putString(final String key, final String value) {
+        tag.putString(key, value);
+        return this;
+    }
 
-		public CompoundTagBuilder putInt(final String key, int value) {
-			tag.putInt(key, value);
-			return this;
-		}
+    public CompoundTagBuilder putInt(final String key, int value) {
+        tag.putInt(key, value);
+        return this;
+    }
 
-		public CompoundTag build() {
-			return tag;
-		}
-	}
+    public CompoundTag build() {
+        return tag;
+    }
+}
