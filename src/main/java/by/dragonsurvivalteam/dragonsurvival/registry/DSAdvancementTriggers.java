87fa062706ad057712fbc25a4a930cce1f9203b3
Psither <97207971+Psither@users.noreply.github.com>
2024-08-25 12:26:56 -0400

    Sleep on treasure advancements

--- src/main/java/by/dragonsurvivalteam/dragonsurvival/registry/DSAdvancementTriggers.java
+++ src/main/java/by/dragonsurvivalteam/dragonsurvival/registry/DSAdvancementTriggers.java
@@ -1,6 +1,7 @@
 package by.dragonsurvivalteam.dragonsurvival.registry;
 
 import by.dragonsurvivalteam.dragonsurvival.common.criteria.BecomeDragonTrigger;
+import by.dragonsurvivalteam.dragonsurvival.common.criteria.SleepOnTreasureTrigger;
 import net.minecraft.advancements.CriterionTrigger;
 import net.minecraft.core.registries.Registries;
 import net.neoforged.neoforge.registries.DeferredRegister;
@@ -15,5 +16,8 @@ public class DSAdvancementTriggers {
 
     public static final Supplier<BecomeDragonTrigger> BECOME_DRAGON = DS_TRIGGERS.register(
             "become_dragon", BecomeDragonTrigger::new
+    );
+    public static final Supplier<SleepOnTreasureTrigger> SLEEP_ON_TREASURE = DS_TRIGGERS.register(
+            "sleep_on_treasure", SleepOnTreasureTrigger::new
     );
 }
