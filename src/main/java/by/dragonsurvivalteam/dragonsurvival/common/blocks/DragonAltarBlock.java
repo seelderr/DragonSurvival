c5ac362183d1cf80bdbf3c5976baa7fec49481d8
Seelder <zlschect@gmail.com>
2024-09-01 13:51:17 -0700

    Fixed dragon altar cooldown being synced incorrectly

--- src/main/java/by/dragonsurvivalteam/dragonsurvival/common/blocks/DragonAltarBlock.java
+++ src/main/java/by/dragonsurvivalteam/dragonsurvival/common/blocks/DragonAltarBlock.java
@@ -4,12 +4,14 @@ package by.dragonsurvivalteam.dragonsurvival.common.blocks;
 import by.dragonsurvivalteam.dragonsurvival.client.gui.screens.DragonAltarScreen;
 import by.dragonsurvivalteam.dragonsurvival.common.capability.DragonStateHandler;
 import by.dragonsurvivalteam.dragonsurvival.common.capability.DragonStateProvider;
+import by.dragonsurvivalteam.dragonsurvival.network.container.AllowOpenDragonAltar;
 import by.dragonsurvivalteam.dragonsurvival.util.Functions;
 import java.util.List;
 import net.minecraft.client.Minecraft;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.network.chat.Component;
+import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
@@ -29,6 +31,7 @@ import net.minecraft.world.phys.shapes.Shapes;
 import net.minecraft.world.phys.shapes.VoxelShape;
 import net.neoforged.api.distmarker.Dist;
 import net.neoforged.api.distmarker.OnlyIn;
+import net.neoforged.neoforge.network.PacketDistributor;
 import org.jetbrains.annotations.NotNull;
 
 
@@ -62,25 +65,23 @@ public class DragonAltarBlock extends Block{
 	public InteractionResult useWithoutItem(@NotNull BlockState pState, @NotNull Level pLevel, @NotNull BlockPos pPos, @NotNull Player pPlayer, @NotNull BlockHitResult pHitResult){
 		DragonStateHandler handler = DragonStateProvider.getOrGenerateHandler(pPlayer);
 
-		if(handler.altarCooldown > 0){
-			if(pLevel.isClientSide()){
+		if(!pLevel.isClientSide()) {
+			if(handler.altarCooldown > 0){
 				//Show the current cooldown in minutes and seconds in cases where the cooldown is set high in the config
 				int mins = (int) (Functions.ticksToMinutes(handler.altarCooldown));
 				int secs = (int) (Functions.ticksToSeconds(handler.altarCooldown - Functions.minutesToTicks(mins)));
 				pPlayer.sendSystemMessage(Component.translatable("ds.cooldown.active", (mins > 0 ? mins + "m" : "") + secs + (mins > 0 ? "s" : "")));
-			}
-			return InteractionResult.CONSUME;
-		}else{
-			if(pLevel.isClientSide()){
-				openGUi();
+				return InteractionResult.FAIL;
+			} else {
+				PacketDistributor.sendToPlayer((ServerPlayer)pPlayer, new AllowOpenDragonAltar.Data());
+				handler.altarCooldown = Functions.minutesToTicks(5);
+				handler.hasUsedAltar = true;
+				handler.isInAltar = true;
+				return InteractionResult.CONSUME;
 			}
 		}
-		return InteractionResult.SUCCESS;
-	}
 
-	@OnlyIn( Dist.CLIENT )
-	private void openGUi(){
-		Minecraft.getInstance().setScreen(new DragonAltarScreen());
+		return InteractionResult.SUCCESS;
 	}
 
 	@Override
