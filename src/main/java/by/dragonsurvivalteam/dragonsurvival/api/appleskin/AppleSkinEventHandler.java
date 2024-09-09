b02a1d11a9c85fec01144381563cd075eb367378
Seelder <zlschect@gmail.com>
2024-09-08 19:58:25 -0700

    Removed hide appleskin tooltip config and fixed appleskin not displaying food data correctly for dragon food

--- src/main/java/by/dragonsurvivalteam/dragonsurvival/api/appleskin/AppleSkinEventHandler.java
+++ src/main/java/by/dragonsurvivalteam/dragonsurvival/api/appleskin/AppleSkinEventHandler.java
@@ -1,19 +1,22 @@
 package by.dragonsurvivalteam.dragonsurvival.api.appleskin;
 
-import by.dragonsurvivalteam.dragonsurvival.client.gui.widgets.ToolTipHandler;
 import by.dragonsurvivalteam.dragonsurvival.common.capability.DragonStateProvider;
+import by.dragonsurvivalteam.dragonsurvival.common.handlers.DragonFoodHandler;
 import net.minecraft.client.Minecraft;
 import net.neoforged.api.distmarker.Dist;
 import net.neoforged.api.distmarker.OnlyIn;
+import net.neoforged.bus.api.EventPriority;
 import net.neoforged.bus.api.SubscribeEvent;
-import squeek.appleskin.api.event.TooltipOverlayEvent;
+import squeek.appleskin.api.event.FoodValuesEvent;
 
 @OnlyIn(Dist.CLIENT)
 public class AppleSkinEventHandler {
-    @SubscribeEvent
-    public void onEvent(final TooltipOverlayEvent.Pre event) {
-        if (ToolTipHandler.hideAppleskinTooltip && DragonStateProvider.isDragon(Minecraft.getInstance().player)) {
-            event.setCanceled(true);
-        }
+    @SubscribeEvent(priority = EventPriority.HIGHEST)
+    public void applyDragonFoodPropertiesToAppleSkin(final FoodValuesEvent event) {
+        DragonStateProvider.getCap(Minecraft.getInstance().player).ifPresent(handler -> {
+            if (handler.isDragon()) {
+                event.modifiedFoodProperties = DragonFoodHandler.getDragonFoodProperties(event.itemStack, handler.getType());
+            }
+        });
     }
 }
