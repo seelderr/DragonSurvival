cba1baabf66020cb340c0834d4c93d5c3e7257ac
Seelder <zlschect@gmail.com>
2024-09-21 17:25:34 -0700

    Removed OnlyIn from the DragonEditorHandler class declaration

--- src/main/java/by/dragonsurvivalteam/dragonsurvival/client/skin_editor_system/DragonEditorHandler.java
+++ src/main/java/by/dragonsurvivalteam/dragonsurvival/client/skin_editor_system/DragonEditorHandler.java
@@ -35,14 +35,12 @@ import net.minecraft.resources.ResourceLocation;
 import net.minecraft.util.Mth;
 import net.minecraft.world.entity.player.Player;
 import net.neoforged.api.distmarker.Dist;
-import net.neoforged.api.distmarker.OnlyIn;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.neoforge.client.GlStateBackup;
 import net.neoforged.neoforge.client.event.RegisterShadersEvent;
 import org.jetbrains.annotations.Nullable;
 
-@OnlyIn(Dist.CLIENT)
 @EventBusSubscriber(bus = EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
 public class DragonEditorHandler{
 	private static ShaderInstance skinGenerationShader;
