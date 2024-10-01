46426fd9cc5e1ed75b508ac58917c619849e9796
Seelder <zlschect@gmail.com>
2024-09-30 18:26:33 -0700

    Fixed a crash when mods use a ThrownPotion item that doesn't actually have any Potion data.

--- src/main/java/by/dragonsurvivalteam/dragonsurvival/util/PotionUtils.java
+++ src/main/java/by/dragonsurvivalteam/dragonsurvival/util/PotionUtils.java
@@ -2,11 +2,22 @@ package by.dragonsurvivalteam.dragonsurvival.util;
 
 import static net.minecraft.core.component.DataComponents.POTION_CONTENTS;
 
+import net.minecraft.core.Holder;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.alchemy.Potion;
+import net.minecraft.world.item.alchemy.PotionContents;
+
+import java.util.Optional;
 
 public class PotionUtils {
-    public static Potion getPotion(ItemStack item) {
-        return item.get(POTION_CONTENTS).potion().get().value();
+    // We need to be careful here, as some mods might have items that are instances of
+    // PotionItem that do not actually have POTION_CONTENTS, which would crash in previous iterations of this code.
+    public static Optional<Potion> getPotion(ItemStack item) {
+        PotionContents potionContents = item.get(POTION_CONTENTS);
+        if (potionContents == null || potionContents.potion().isEmpty()) {
+            return Optional.empty();
+        }
+
+        return Optional.of(potionContents.potion().get().value());
     }
 }
