e8764770d7ae21c2058049b82def36935d7d8999
Metaflame <metaflamedragon@gmail.com>
2024-10-15 03:17:13 +0200

    Reformat Functions

--- src/main/java/by/dragonsurvivalteam/dragonsurvival/util/Functions.java
+++ src/main/java/by/dragonsurvivalteam/dragonsurvival/util/Functions.java
@@ -12,79 +12,80 @@ import net.minecraft.world.entity.ai.attributes.Attributes;
 import net.minecraft.world.entity.player.Player;
 import org.joml.Vector3f;
 
-public class Functions{
-	public static int minutesToTicks(int minutes){
-		return secondsToTicks(minutes) * 60;
-	}
+public class Functions {
+    public static int minutesToTicks(int minutes) {
+        return secondsToTicks(minutes) * 60;
+    }
 
-	public static int secondsToTicks(double seconds) {
-		return (int) (seconds * 20);
-	}
-	
-	public static int secondsToTicks(int seconds){
-		return seconds * 20;
-	}
+    public static int secondsToTicks(double seconds) {
+        return (int) (seconds * 20);
+    }
 
-	public static double ticksToMinutes(int ticks){
-		return ticksToSeconds(ticks) / 60;
-	}
+    public static int secondsToTicks(int seconds) {
+        return seconds * 20;
+    }
 
-	public static double ticksToSeconds(int ticks){
-		return ticks / 20d;
-	}
+    public static double ticksToMinutes(int ticks) {
+        return ticksToSeconds(ticks) / 60;
+    }
 
-	public static float angleDifference(float b, float a) {
-		return Mth.wrapDegrees(b - a);
-	}
+    public static double ticksToSeconds(int ticks) {
+        return ticks / 20d;
+    }
 
-	public static double angleDifference(double b, double a) {
-		return Mth.wrapDegrees(b - a);
-	}
-	
-	
-	public static ListTag newDoubleList(double... pNumbers) {
-		ListTag listtag = new ListTag();
+    public static float angleDifference(float b, float a) {
+        return Mth.wrapDegrees(b - a);
+    }
 
-		for(double d0 : pNumbers) {
-			listtag.add(DoubleTag.valueOf(d0));
-		}
+    public static double angleDifference(double b, double a) {
+        return Mth.wrapDegrees(b - a);
+    }
 
-		return listtag;
-	}
 
-	/**
-	 * Returns a new NBTTagList filled with the specified floats
-	 */
-	public static ListTag newFloatList(float... pNumbers) {
-		ListTag listtag = new ListTag();
+    public static ListTag newDoubleList(double... pNumbers) {
+        ListTag listtag = new ListTag();
 
-		for(float f : pNumbers) {
-			listtag.add(FloatTag.valueOf(f));
-		}
+        for (double d0 : pNumbers) {
+            listtag.add(DoubleTag.valueOf(d0));
+        }
 
-		return listtag;
-	}
-	
-	public static int wrap(int value, int min, int max){
-		return value < min ? max : value > max ? min : value;
-	}
-	
-	public static Vector3f getDragonCameraOffset(Entity entity){
-		Vector3f lookVector = new Vector3f(0, 0, 0);
+        return listtag;
+    }
 
-		if(entity instanceof Player player){
-			DragonStateHandler handler = DragonStateProvider.getOrGenerateHandler(player);
-			if(handler.isDragon()){
-				float f1 = -(float)handler.getMovementData().bodyYaw * ((float)Math.PI / 180F);
+    /**
+     * Returns a new NBTTagList filled with the specified floats
+     */
+    public static ListTag newFloatList(float... pNumbers) {
+        ListTag listtag = new ListTag();
 
-				float f4 = Mth.sin(f1);
-				float f5 = Mth.cos(f1);
-				AttributeInstance attributeInstance = player.getAttribute(Attributes.SCALE);
-				double scale = attributeInstance != null ? attributeInstance.getValue() : 1.0d;
-				lookVector.set((float)(f4 * (handler.getSize() * scale / 40)), 0, (float)(f5 * (handler.getSize() * scale / 40)));
-			}
-		}
+        for (float f : pNumbers) {
+            listtag.add(FloatTag.valueOf(f));
+        }
 
-		return lookVector;
-	}
+
+        return listtag;
+    }
+
+    public static int wrap(int value, int min, int max) {
+        return value < min ? max : value > max ? min : value;
+    }
+
+    public static Vector3f getDragonCameraOffset(Entity entity) {
+        Vector3f lookVector = new Vector3f(0, 0, 0);
+
+        if (entity instanceof Player player) {
+            DragonStateHandler handler = DragonStateProvider.getOrGenerateHandler(player);
+            if (handler.isDragon()) {
+                float f1 = -(float) handler.getMovementData().bodyYaw * ((float) Math.PI / 180F);
+
+                float f4 = Mth.sin(f1);
+                float f5 = Mth.cos(f1);
+                AttributeInstance attributeInstance = player.getAttribute(Attributes.SCALE);
+                double scale = attributeInstance != null ? attributeInstance.getValue() : 1.0d;
+                lookVector.set((float) (f4 * (handler.getSize() * scale / 40)), 0, (float) (f5 * (handler.getSize() * scale / 40)));
+            }
+        }
+
+        return lookVector;
+    }
 }
