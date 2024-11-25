de455868e26c1758bb22066fb78f671f87f20f77
Kai <kai.zahn@yahoo.de>
2024-11-25 12:52:43 +0100

    ability instance?

--- src/main/java/by/dragonsurvivalteam/dragonsurvival/registry/dragon/AbilityEffect.java
+++ src/main/java/by/dragonsurvivalteam/dragonsurvival/registry/dragon/AbilityEffect.java
@@ -1,10 +1,10 @@
 package by.dragonsurvivalteam.dragonsurvival.registry.dragon;
 
-import by.dragonsurvivalteam.dragonsurvival.registry.dragon.ability.DragonAbility;
+import by.dragonsurvivalteam.dragonsurvival.registry.dragon.ability.DragonAbilityInstance;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.phys.Vec3;
 
 public interface AbilityEffect {
-    void apply(final ServerLevel level, final Player player, DragonAbility ability, Vec3 origin);
+    void apply(final ServerLevel level, final Player player, final DragonAbilityInstance ability, final Vec3 origin);
 }
--- src/main/java/by/dragonsurvivalteam/dragonsurvival/registry/dragon/ability/DragonAbilityInstance.java
+++ src/main/java/by/dragonsurvivalteam/dragonsurvival/registry/dragon/ability/DragonAbilityInstance.java
@@ -1,12 +1,34 @@
 package by.dragonsurvivalteam.dragonsurvival.registry.dragon.ability;
 
 import net.minecraft.core.Holder;
+import net.minecraft.core.HolderLookup;
+import net.minecraft.nbt.CompoundTag;
+import net.neoforged.neoforge.common.util.INBTSerializable;
+import org.jetbrains.annotations.NotNull;
 
-public class DragonAbilityInstance {
+public class DragonAbilityInstance implements INBTSerializable<CompoundTag> {
     private final Holder<DragonAbility> ability;
     private int level;
+
+    // TODO :: store cooldown here?
 
     public DragonAbilityInstance(final Holder<DragonAbility> ability) {
         this.ability = ability;
     }
+
+    // TODO :: could also handle this through a CODEC defined for the instance (like MobEffectInstance)
+    @Override
+    public CompoundTag serializeNBT(@NotNull final HolderLookup.Provider provider) {
+        CompoundTag tag = new CompoundTag();
+        tag.putInt(LEVEL, level);
+
+        return tag;
+    }
+
+    @Override
+    public void deserializeNBT(@NotNull final HolderLookup.Provider provider, @NotNull final CompoundTag tag) {
+        this.level = tag.getInt(LEVEL);
+    }
+
+    private final String LEVEL = "level";
 }
