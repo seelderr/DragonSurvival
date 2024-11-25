da286bd7ce4c2bd46d47add5f139af2a6e1da9c4
Kai <kai.zahn@yahoo.de>
2024-11-25 11:41:31 +0100

    add comments

--- src/main/java/by/dragonsurvivalteam/dragonsurvival/common/codecs/ability/Activation.java
+++ src/main/java/by/dragonsurvivalteam/dragonsurvival/common/codecs/ability/Activation.java
@@ -10,15 +10,21 @@ public record Activation(Type type, int castTime, int cooldown) {
     public static final int NO_COOLDOWN = 0;
 
     public static final Codec<Activation> CODEC = RecordCodecBuilder.create(instance -> instance.group(
-            Type.CODEC.fieldOf("type").forGetter(Activation::type),
+            Type.CODEC.optionalFieldOf("type", Type.NONE).forGetter(Activation::type),
             // TODO :: validate
             Codec.INT.optionalFieldOf("cast_time", INSTANT).forGetter(Activation::castTime),
-            Codec.INT.optionalFieldOf("cast_time", NO_COOLDOWN).forGetter(Activation::cooldown)
+            Codec.INT.optionalFieldOf("cooldown", NO_COOLDOWN).forGetter(Activation::cooldown)
     ).apply(instance, Activation::new));
 
+    // Generally the logic triggers on 'InputEvent.Key' when they key KEY_PRESSED
     public enum Type implements StringRepresentable {
+        // If the cast time has passed the 'InputEvent.Key' will trigger the existing effects while they key is KEY_HELD
         CHANNELED("channeled"),
-        TOGGLED("charged");
+        // If the cast time has passed a boolean (in a map?) gets switched
+        // While that boolean is active the effects will try to run based on their 'Application'
+        TOGGLED("charged"),
+        // If the cast time has passed the effects will trigger once
+        NONE("none");
 
         public static final Codec<Type> CODEC = StringRepresentable.fromEnum(Type::values);
 
