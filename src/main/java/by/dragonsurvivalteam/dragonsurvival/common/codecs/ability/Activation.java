52f771ef87dac6f55d9ca19c803c9e16e49a5551
Kai <kai.zahn@yahoo.de>
2024-11-25 11:03:26 +0100

    adjust activation

--- src/main/java/by/dragonsurvivalteam/dragonsurvival/common/codecs/ability/Activation.java
+++ src/main/java/by/dragonsurvivalteam/dragonsurvival/common/codecs/ability/Activation.java
@@ -5,19 +5,20 @@ import com.mojang.serialization.codecs.RecordCodecBuilder;
 import net.minecraft.util.StringRepresentable;
 import org.jetbrains.annotations.NotNull;
 
-import java.util.Optional;
+public record Activation(Type type, int castTime, int cooldown) {
+    public static final int INSTANT = -1;
+    public static final int NO_COOLDOWN = 0;
 
-public record Activation(Type type, Optional<Data> data) {
     public static final Codec<Activation> CODEC = RecordCodecBuilder.create(instance -> instance.group(
             Type.CODEC.fieldOf("type").forGetter(Activation::type),
-            // Can we use 'Codec.either()' to map this two different codecs depending on the type...?
-            // Since 'CHANNELED' should not have the option to define 'toggled'
-            Data.CODEC.optionalFieldOf("data").forGetter(Activation::data)
+            // TODO :: validate
+            Codec.INT.optionalFieldOf("cast_time", INSTANT).forGetter(Activation::castTime),
+            Codec.INT.optionalFieldOf("cast_time", NO_COOLDOWN).forGetter(Activation::cooldown)
     ).apply(instance, Activation::new));
 
     public enum Type implements StringRepresentable {
         CHANNELED("channeled"),
-        CHARGED("charged"); // If the Data::time is 0 then it is INSTANT
+        TOGGLED("charged");
 
         public static final Codec<Type> CODEC = StringRepresentable.fromEnum(Type::values);
 
@@ -31,12 +32,5 @@ public record Activation(Type type, Optional<Data> data) {
         public @NotNull String getSerializedName() {
             return name;
         }
-    }
-
-    public record Data(int time, boolean toggled) {
-        public static final Codec<Data> CODEC = RecordCodecBuilder.create(instance -> instance.group(
-                Codec.INT.fieldOf("time").forGetter(Data::time),
-                Codec.BOOL.optionalFieldOf("toggled", false).forGetter(Data::toggled)
-        ).apply(instance, Data::new));
     }
 }
--- src/main/java/by/dragonsurvivalteam/dragonsurvival/registry/dragon/DragonType.java
+++ src/main/java/by/dragonsurvivalteam/dragonsurvival/registry/dragon/DragonType.java
@@ -5,9 +5,14 @@ import by.dragonsurvivalteam.dragonsurvival.common.codecs.MiscCodecs;
 import by.dragonsurvivalteam.dragonsurvival.common.codecs.ability.Penalty;
 import com.mojang.serialization.Codec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
+import net.minecraft.core.Holder;
 import net.minecraft.core.HolderSet;
 import net.minecraft.core.Registry;
 import net.minecraft.core.RegistryCodecs;
+import net.minecraft.network.RegistryFriendlyByteBuf;
+import net.minecraft.network.codec.ByteBufCodecs;
+import net.minecraft.network.codec.StreamCodec;
+import net.minecraft.resources.RegistryFixedCodec;
 import net.minecraft.resources.ResourceKey;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
@@ -25,11 +30,16 @@ public record DragonType(
     public static final ResourceKey<Registry<DragonType>> REGISTRY = ResourceKey.createRegistryKey(DragonSurvival.res("dragon_types"));
 
     public static final Codec<DragonType> DIRECT_CODEC = RecordCodecBuilder.create(instance -> instance.group(
-            RegistryCodecs.homogeneousList(DragonStage.REGISTRY).fieldOf("stages").forGetter(DragonType::stages),
-            RegistryCodecs.homogeneousList(DragonBody.REGISTRY).fieldOf("bodies").forGetter(DragonType::bodies),
+            // No defined stages means all are applicable
+            RegistryCodecs.homogeneousList(DragonStage.REGISTRY).optionalFieldOf("stages", HolderSet.empty()).forGetter(DragonType::stages),
+            // No defined bodies means all are applicable
+            RegistryCodecs.homogeneousList(DragonBody.REGISTRY).optionalFieldOf("bodies", HolderSet.empty()).forGetter(DragonType::bodies),
             MiscCodecs.dragonAbilityCodec().optionalFieldOf("abilities", HolderSet.empty()).forGetter(DragonType::abilities),
             Penalty.CODEC.listOf().optionalFieldOf("penalties", List.of()).forGetter(DragonType::penalties)
     ).apply(instance, instance.stable(DragonType::new)));
+
+    public static final Codec<Holder<DragonType>> CODEC = RegistryFixedCodec.create(REGISTRY);
+    public static final StreamCodec<RegistryFriendlyByteBuf, Holder<DragonType>> STREAM_CODEC = ByteBufCodecs.holderRegistry(REGISTRY);
 
     @SubscribeEvent
     public static void register(final DataPackRegistryEvent.NewRegistry event) {
