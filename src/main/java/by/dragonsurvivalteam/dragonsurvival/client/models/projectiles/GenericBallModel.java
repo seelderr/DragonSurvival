816cc1d28d50da02c49e1090b22d1bd531e4756b
Kai <kai.zahn@yahoo.de>
2024-11-27 07:46:09 +0100

    comment + (imo static import should only be used if we a have a lot of usages for it in the class or the line gets really long)

--- src/main/java/by/dragonsurvivalteam/dragonsurvival/client/models/projectiles/GenericBallModel.java
+++ src/main/java/by/dragonsurvivalteam/dragonsurvival/client/models/projectiles/GenericBallModel.java
@@ -5,26 +5,24 @@ import by.dragonsurvivalteam.dragonsurvival.common.entity.projectiles.GenericBal
 import net.minecraft.resources.ResourceLocation;
 import software.bernie.geckolib.model.GeoModel;
 
-import static by.dragonsurvivalteam.dragonsurvival.DragonSurvival.MODID;
-
 public class GenericBallModel extends GeoModel<GenericBallEntity> {
     // TODO: Make this dynamically work correctly
 
-    @Override
+    @Override // TODO :: should these use a custom namespace?
     public ResourceLocation getModelResource(GenericBallEntity animatable) {
-        return ResourceLocation.fromNamespaceAndPath(MODID, "geo/dragon_lightning_ball.geo.json");
+        return DragonSurvival.res("geo/dragon_lightning_ball.geo.json");
        // return ResourceLocation.fromNamespaceAndPath(DragonSurvival.MODID, "geo/" + animatable.getName() + ".geo.json");
     }
 
     @Override
     public ResourceLocation getTextureResource(GenericBallEntity animatable) {
-        return ResourceLocation.fromNamespaceAndPath(MODID, "textures/entity/lightning_texture.png");
+        return DragonSurvival.res("textures/entity/lightning_texture.png");
         //return ResourceLocation.fromNamespaceAndPath(DragonSurvival.MODID, "textures/entity/" + animatable.getName() + ".png");
     }
 
     @Override
     public ResourceLocation getAnimationResource(GenericBallEntity animatable) {
-        return ResourceLocation.fromNamespaceAndPath(MODID, "animations/lightning_ball.animation.json");
+        return DragonSurvival.res("animations/lightning_ball.animation.json");
         //return ResourceLocation.fromNamespaceAndPath(DragonSurvival.MODID, "animations/" + animatable.getName() + ".animation.json");
     }
 }
