5e187e8b6e6f06537f65605ae76c37bfef504d5f
seelder <zlschect@gmail.com>
2024-11-26 21:00:29 -0500

    Marked comment on ProjectileLightningEffect

--- src/main/java/by/dragonsurvivalteam/dragonsurvival/common/codecs/projectile/world_effects/ProjectileLightningEffect.java
+++ src/main/java/by/dragonsurvivalteam/dragonsurvival/common/codecs/projectile/world_effects/ProjectileLightningEffect.java
@@ -1,7 +1,7 @@
 package by.dragonsurvivalteam.dragonsurvival.common.codecs.projectile.world_effects;
 
 import by.dragonsurvivalteam.dragonsurvival.common.codecs.projectile.ProjectileInstance;
-import by.dragonsurvivalteam.dragonsurvival.common.codecs.projectile.targeting.ProjectileTargeting;
+import com.mojang.serialization.Codec;
 import com.mojang.serialization.MapCodec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
 import net.minecraft.server.level.ServerLevel;
@@ -10,10 +10,11 @@ import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.LightningBolt;
 import net.minecraft.world.phys.Vec3;
 
-public record ProjectileLightningEffect(ProjectileTargeting.WorldTargeting targeting) implements ProjectileWorldEffect {
+// TODO: How to make identity codec here?
+public record ProjectileLightningEffect(int unused) implements ProjectileWorldEffect {
 
     public static final MapCodec<ProjectileLightningEffect> CODEC = RecordCodecBuilder.mapCodec(instance -> instance.group(
-            ProjectileTargeting.WorldTargeting.CODEC.fieldOf("target").forGetter(ProjectileLightningEffect::targeting)
+            Codec.INT.fieldOf("unused").forGetter(ProjectileLightningEffect::unused)
     ).apply(instance, ProjectileLightningEffect::new));
 
     @Override
